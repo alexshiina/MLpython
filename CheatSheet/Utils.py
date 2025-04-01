800bf26b53715a5d996fa9764a1cee83717f7b90
alexshiina <aleksgoly3@gmail.com>
2025-04-01 17:14:20 +0200

    fixed save_model

--- CheatSheet/Utils.py
+++ CheatSheet/Utils.py
@@ -8,29 +8,29 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 def save_model(model:torch.nn.Module,
-               targed_dir: str,
+               target_dir: str,
                model_name: str):
   """
   Saves a PyTorch model to a target directory.
 
   Args:
   model: Target model.
-  targed_dir: Directory for saving the model to.
+  target_dir: Directory for saving the model to.
   model_name: A filename for the saved model. Should include
   either ".pth" or ".pt" as the file extension.
   """
   #creating directory
-  targed_dir_path = Path(targed_dir)
-  targed_dir.mkdir(parents = True,
+  target_dir_path = Path(target_dir)
+  target_dir.mkdir(parents = True,
                    exist_ok = True)
 
   #creating model save path
   assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model name should end with '.pt' or '.pth'"
-  model_save_path = targed_dir_path/model_name
+  model_save_path = target_dir_path/model_name
 
   #saving
   print(f"[INFO] Saving model to: {model_save_path}")
-  torch.save(obj=model.load_state_dict(),
+  torch.save(obj=model.state_dict(),
              f=model_save_path)
 
 #function from mrdbourke's helper_functions.py
